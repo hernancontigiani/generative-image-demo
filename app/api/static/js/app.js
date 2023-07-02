44560a55f7ee6e58d044e00100d5c68078326b26
hernancontigiani <hernan4790@gmail.com>
2023-07-02 10:10:06 -0300

    Use query parameters

--- app/api/static/js/app.js
+++ app/api/static/js/app.js
@@ -15,15 +15,25 @@ boton.onclick = async () => {
 
     // Loading image...
     imageOutput.src = "/static/images/loading.jpg";
+
+    const params = {
+        caption: caption,
+    };
+    const query = Object.keys(params)
+                    .map(k => encodeURIComponent(k) + '=' + encodeURIComponent(params[k]))
+                    .join('&');
     
-    const url = `/api/v1.0/predict/${caption}`;
-    const resp = await fetch(url);
+    const url = "/api/v1.0/predict";
+    const resp = await fetch(url + "?" + query);
     if(resp.ok) {
         const data = await resp.blob();
         const imagenURL = URL.createObjectURL(data);
         imageOutput.src = imagenURL;
     } else {
         // Request fail
+        // Clean image
+        imageOutput.src = "";
+        // Output error message
         const data = await resp.json();
         alert(data["detail"]);
     }
